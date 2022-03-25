626d9a89ce4c9109e8a56c1ebfb58dce6baf4cbf
Mauricio Erick Soria Cisneros <100866034+Rickmau@users.noreply.github.com>
2022-03-25 11:06:44 -0600

    Commit from second step

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,3 +1,39 @@
 public class Calculadora {
+    String marca;
+    boolean esCientifica;
+    double primerValor;
+    double segundoValor;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public boolean isEsCientifica() {
+        return esCientifica;
+    }
+
+    public void setEsCientifica(boolean esCientifica) {
+        this.esCientifica = esCientifica;
+    }
+
+    public double getPrimerValor() {
+        return primerValor;
+    }
+
+    public void setPrimerValor(double primerValor) {
+        this.primerValor = primerValor;
+    }
+
+    public double getSegundoValor() {
+        return segundoValor;
+    }
+
+    public void setSegundoValor(double segundoValor) {
+        this.segundoValor = segundoValor;
+    }
 
 }
