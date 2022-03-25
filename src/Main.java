40601c086677a35addcfe0d86b0b5a8c464c94e4
Mauricio Erick Soria Cisneros <100866034+Rickmau@users.noreply.github.com>
2022-03-25 12:27:26 -0600

    Commit from fifth step

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        Calculadora basica = new Calculadora();
+        basica.setMarca("patito");
+        basica.setEsCientifica(false);
+        basica.setPrimerValor(294.9);
+        basica.setSegundoValor(0.0);
+
+        basica.showSuma(basica.suma());
+        basica.showResta(basica.resta());
+        basica.showMult(basica.multiplicacion());
+        System.out.println(basica.division());
+        System.out.println(basica.elevarAPotencia());
+        System.out.println(basica.toString());
+
+        Calculadora cientifica = new Calculadora();
+        cientifica.setMarca("casio");
+        cientifica.setEsCientifica(true);
+        cientifica.setPrimerValor(45.83);
+        cientifica.setSegundoValor(13.6);
+
+        cientifica.showSuma(basica.suma());
+        cientifica.showResta(basica.resta());
+        cientifica.showMult(basica.multiplicacion());
+        System.out.println(cientifica.division());
+        System.out.println(cientifica.elevarAPotencia());
+        System.out.println(cientifica.toString());
+
 
     }
 }
